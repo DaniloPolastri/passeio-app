60433fda602deb4eccf5cb50844581caa24371e9
Danilo <danilopolastri.ti@gmail.com>
2025-06-19 16:30:52 -0300

    Initial commit

---
+++ src/app/categorias/categoria/categoria.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+
+@Component({
+  selector: 'app-categoria',
+  standalone: false,
+  templateUrl: './categoria.html',
+  styleUrl: './categoria.scss'
+})
+export class Categoria {
+  camposForm: FormGroup;
+
+  constructor() {
+    this.camposForm = new FormGroup({
+      nome: new FormControl('', Validators.required),
+      descricao: new FormControl('', Validators.required),
+    })
+  }
+
+  salvar(){
+    this.camposForm.markAllAsTouched();
+
+    if(this.camposForm.valid){
+      console.log(this.camposForm.value)
+      console.log('Esta valido? ', this.camposForm.valid);
+    }
+
+  }
+
+  isCampoInvalido(nomeCampo:string): boolean {
+    const campo = this.camposForm.get(nomeCampo);
+    return (campo?.invalid && campo.touched && campo?.errors?.['required']) || false;
+  }
+}
